9b42798713a0b4184d05b6de778cf1b53da7f28c
Hoang Tuan <hoangtuanictvn@gmail.com>
2018-10-07 03:05:37 +0700

    Update signup params

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    confirmations:      'devise_token_auth/confirmations',
-    passwords:          'devise_token_auth/passwords',
-    omniauth_callbacks: 'devise_token_auth/omniauth_callbacks',
-    registrations:      'devise_token_auth/registrations',
-    sessions:           'devise_token_auth/sessions',
-    token_validations:  'devise_token_auth/token_validations'
+  mount_devise_token_auth_for "User", at: "auth", controllers: {
+    confirmations:      "auth/confirmations",
+    passwords:          "auth/passwords",
+    omniauth_callbacks: "auth/omniauth_callbacks",
+    registrations:      "auth/registrations",
+    sessions:           "auth/sessions",
+    token_validations:  "auth/token_validations"
   }
   root to: "pages#root"
   get "*path", to: "pages#root"
