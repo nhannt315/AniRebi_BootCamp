4a9d196794926736c44d1e05f1f4379ac3ad34db
Hoang Tuan <hoangtuanictvn@gmail.com>
2018-10-07 03:05:37 +0700

    Add devise authentication api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery with: :exception, if: :verify_api
+
+  private
+  def verify_api
+    params[:controller].split('/')[0] != 'devise_token_auth'
+  end
 end
