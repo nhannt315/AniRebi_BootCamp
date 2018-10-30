5a2e77abf556f3db018e4489f9a46a3b459cb8be
bacha277 <bhta277@gmail.com>
2018-10-30 21:45:08 +0700

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'ffaker'
+
+review_count = 10
+
+review_list = Array.new
+
+for i in 1..review_count
+  review_list.push({
+    user_id: User.all.ids.sample,
+    anime_id: Anime.all.ids.sample,
+    title: FFaker::Tweet.tweet,
+    content: FFaker::LoremRU.paragraph,
+    rating: (rand(1..10).to_f)/2
+    })
+end
+
+review_list.each do |review|
+  Review.create(review)
+end
