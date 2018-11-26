b3958d6a7c302a52a67bbe167396ad02d0d761cd
yungwiche <le.minhhoang@outlook.com>
2018-11-27 01:41:47 +0700

    Added suggested anime and sort by box.

--- app/javascript/store/sagas/anime_saga.js
+++ app/javascript/store/sagas/anime_saga.js
@@ -139,7 +139,15 @@ export function* getMultipleGenreTop(action) {
 export function* getReviewsByAnime(action) {
   yield put(actions.getReviewsByAnimeStart());
   try {
-    let url = endpoints.GET_REVIEWS_BY_ANIME + `?id=${action.id}`;
+    let url;
+    if (action.order) {
+      url =
+        endpoints.GET_REVIEWS_BY_ANIME +
+        `?id=${action.id}` +
+        `&order=${action.order}`;
+    } else {
+      url = endpoints.GET_REVIEWS_BY_ANIME + `?id=${action.id}`;
+    }
     console.log(url);
     const response = yield axios.get(url);
     // console.log(response.data);
